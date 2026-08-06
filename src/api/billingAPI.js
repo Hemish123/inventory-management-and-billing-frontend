742f1786f5f99c42a429515faf5862bf65cbfb0f
Hemish123 <consultant@jmsadvisory.in>
2026-08-06 15:52:38 +0530

    bill pdf url change

--- src/api/billingAPI.js
+++ src/api/billingAPI.js
@@ -25,3 +25,6 @@ export const finalizeBill = (id, data = {}) =>
 
 export const discardDraft = (id) =>
   apiCall(() => api.delete(`/billing/${id}/discard/`));
+
+export const getReceiptPDFBlob = (id) =>
+  api.get(`/billing/${id}/pdf/`, { responseType: 'blob' });
